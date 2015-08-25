65161ae47de3228c8d216b4516838dd69009400d
Dio Mercury <dio@live.ru>
2015-08-25 19:27:10 +0300

    Fixing bug with nested keys

--- index.js
+++ index.js
@@ -6,72 +6,69 @@ var through = require('through2');
 var semver = require('semver');
 var Dot = require('dot-object');
 
-module.exports = function(opts) {
-  // set task options
-  opts = setDefaultOptions(opts);
+module.exports = function (opts) {
+  var dot = new Dot();
 
-  var content, json, ver;
+    // set task options
+    opts = setDefaultOptions(opts);
 
-  return through.obj(function(file, enc, cb) {
-    if (file.isNull()) {
-      return cb(null, file);
-    }
-    if (file.isStream()) {
-      return cb(new gutil.PluginError('gulp-bump', 'Streaming not supported'));
-    }
+    var content, json, ver;
 
-    json = file.contents.toString();
-    try {
-      content = JSON.parse(json);
-    } catch (e) {
-      return cb(new gutil.PluginError('gulp-bump', 'Problem parsing JSON file', {fileName: file.path, showStack: true}));
-    }
-
-    // just set a version to the key
-    if (opts.version) {
-      if (!content[opts.key]) {
-        // log to user that key didn't exist before
-        gutil.log('Creating key', gutil.colors.red(opts.key), 'with version:', gutil.colors.cyan(opts.version));
+    return through.obj(function (file, enc, cb) {
+      if (file.isNull()) {
+        return cb(null, file);
       }
-      content[opts.key] = opts.version;
-      ver = content[opts.key];
-    }
-    else if (semver.valid(content[opts.key])) {
-    // increment the key with type
-      content[opts.key] = semver.inc(content[opts.key], opts.type, opts.preid);
-      ver = content[opts.key];
-    }
-    else if (opts.key.indexOf('.') > -1) {
-      var dot = new Dot();
-      var value = dot.pick(opts.key, content);
-      ver = semver.inc(value, opts.type, opts.preid);
-      dot.str(opts.key, ver, content);
-    }
-    else {
-      return cb(new gutil.PluginError('gulp-bump', 'Detected invalid semver ' + opts.key, {fileName: file.path, showStack: false}));
-    }
-    file.contents = new Buffer(JSON.stringify(content, null, opts.indent || space(json)) + possibleNewline(json));
+      if (file.isStream()) {
+        return cb(new gutil.PluginError('gulp-bump', 'Streaming not supported'));
+      }
 
-    gutil.log('Bumped \'' + gutil.colors.cyan(path.basename(file.path)) + '\' ' + gutil.colors.magenta(opts.key) + ' to: ' + gutil.colors.cyan(ver));
-    cb(null, file);
-  });
+      json = file.contents.toString();
+      try {
+        content = JSON.parse(json);
+      } catch (e) {
+        return cb(new gutil.PluginError('gulp-bump', 'Problem parsing JSON file', { fileName: file.path, showStack: true }));
+      }
+
+      ver = dot.pick(opts.key, content);
+        // just set a version to the key
+        if (opts.version) {
+          if (!ver) {
+                // log to user that key didn't exist before
+                gutil.log('Creating key', gutil.colors.red(opts.key), 'with version:', gutil.colors.cyan(opts.version));
+              }
+              ver = opts.version;
+              dot.str(opts.key, ver, content);
+            }
+            else if (semver.valid(ver)) {
+            // increment the key with type
+            ver = semver.inc(ver, opts.type, opts.preid);
+            dot.str(opts.key, ver, content);
+          }
+          else {
+            return cb(new gutil.PluginError('gulp-bump', 'Detected invalid semver ' + opts.key, { fileName: file.path, showStack: false }));
+          }
+          file.contents = new Buffer(JSON.stringify(content, null, opts.indent || space(json)) + possibleNewline(json));
+
+          gutil.log('Bumped \'' + gutil.colors.cyan(path.basename(file.path)) + '\' ' + gutil.colors.magenta(opts.key) + ' to: ' + gutil.colors.cyan(ver));
+          cb(null, file);
+        });
 };
 
 function setDefaultOptions(opts) {
   opts = opts || {};
   opts.key = opts.key || 'version';
   opts.indent = opts.indent || void 0;
-  // default type bump is patch
-  if (!opts.type || !semver.inc('0.0.1', opts.type)) {
-    opts.type = 'patch';
+    // default type bump is patch
+    if (!opts.type || !semver.inc('0.0.1', opts.type)) {
+      opts.type = 'patch';
+    }
+    // if passed specific version - validate it
+    if (opts.version && !semver.valid(opts.version, opts.type)) {
+      gutil.log('invalid version used as option', gutil.colors.red(opts.version));
+      opts.version = null;
+    }
+    return opts;
   }
-  // if passed specific version - validate it
-  if (opts.version && !semver.valid(opts.version, opts.type)) {
-    gutil.log('invalid version used as option', gutil.colors.red(opts.version));
-    opts.version = null;
-  }
-  return opts;
-}
 
 // Preserver new line at the end of a file
 function possibleNewline(json) {
