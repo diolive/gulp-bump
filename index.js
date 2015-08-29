80000202d3a6ae25a653f0dee14b68d62a97dec7
dio <dio@live.ru>
2015-08-29 20:30:23 +0300

    opts array support. 0.4.0

--- index.js
+++ index.js
@@ -6,50 +6,54 @@ var through = require('through2');
 var semver = require('semver');
 var Dot = require('dot-object');
 
-module.exports = function (opts) {
-  var dot = new Dot();
-
-  // set task options
-  opts = setDefaultOptions(opts);
-
-  var content, json, ver;
+module.exports = function (options) {
+  if (!Array.isArray(options)) {
+    options = [options];
+  }
 
   return through.obj(function (file, enc, cb) {
+    var dot = new Dot(),
+      opts, content, json, ver;
+
     if (file.isNull()) {
       return cb(null, file);
     }
     if (file.isStream()) {
-      return cb(new gutil.PluginError('gulp-bump', 'Streaming not supported'));
+      return cb(new gutil.PluginError('diolive-bump', 'Streaming not supported'));
     }
 
     json = file.contents.toString();
     try {
       content = JSON.parse(json);
     } catch (e) {
-      return cb(new gutil.PluginError('gulp-bump', 'Problem parsing JSON file', { fileName: file.path, showStack: true }));
+      return cb(new gutil.PluginError('diolive-bump', 'Problem parsing JSON file', { fileName: file.path, showStack: true }));
     }
 
-    ver = dot.pick(opts.key, content);
+    for (var i = 0; i < options.length; i++) {
+      opts = setDefaultOptions(options[i]);
 
-    if (opts.version) {
-      if (!ver) {
-        // log to user that key didn't exist before
-        gutil.log('Creating key', gutil.colors.red(opts.key), 'with version:', gutil.colors.cyan(opts.version));
+      ver = dot.pick(opts.key, content);
+
+      if (opts.version) {
+        if (!ver) {
+          // log to user that key didn't exist before
+          gutil.log('Creating key', gutil.colors.green(opts.key), 'with version:', gutil.colors.cyan(opts.version));
+        }
+        ver = opts.version;
+        dot.str(opts.key, ver, content);
       }
-      ver = opts.version;
-      dot.str(opts.key, ver, content);
-    }
-    else if (semver.valid(ver)) {
-      // increment the key with type
-      ver = semver.inc(ver, opts.type, opts.preid);
-      dot.str(opts.key, ver, content);
-    }
-    else {
-      return cb(new gutil.PluginError('gulp-bump', 'Detected invalid semver ' + opts.key, { fileName: file.path, showStack: false }));
+      else if (semver.valid(ver)) {
+        // increment the key with type
+        ver = semver.inc(ver, opts.type, opts.preid);
+        dot.str(opts.key, ver, content);
+      }
+      else {
+        return cb(new gutil.PluginError('diolive-bump', 'Detected invalid semver ' + opts.key, { fileName: file.path, showStack: false }));
+      }
+
+      gutil.log('Bumped \'' + gutil.colors.cyan(path.basename(file.path)) + '\' ' + gutil.colors.magenta(opts.key) + ' to: ' + gutil.colors.cyan(ver));
     }
     file.contents = new Buffer(JSON.stringify(content, null, opts.indent || space(json)) + possibleNewline(json));
-
-    gutil.log('Bumped \'' + gutil.colors.cyan(path.basename(file.path)) + '\' ' + gutil.colors.magenta(opts.key) + ' to: ' + gutil.colors.cyan(ver));
     cb(null, file);
   });
 };
@@ -58,13 +62,15 @@ function setDefaultOptions(opts) {
   opts = opts || {};
   opts.key = opts.key || 'version';
   opts.indent = opts.indent || void 0;
+
   // default type bump is patch
-  if (!opts.type || !semver.inc('0.0.1', opts.type)) {
+  if (!opts.version && (!opts.type || !semver.inc('0.0.1', opts.type))) {
     opts.type = 'patch';
   }
+
   // if passed specific version - validate it
   if (opts.version && !semver.valid(opts.version, opts.type)) {
-    gutil.log('Invalid version used as option', gutil.colors.red(opts.version));
+    gutil.log('Potentially wrong version:', gutil.colors.yellow(opts.version));
   }
   return opts;
 }
